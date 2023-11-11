95310ef976b1ea34872fb583c53dad97d539d887
Nil <95134227+NirejBatrai@users.noreply.github.com>
2023-11-11 20:40:35 +0700

    some changes on text

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,6 +1,6 @@
 export default function GameOver ({winner, onRestart}) {
     return <div id="game-over">
-        <h2>ARE YOU MADE!!! Game Over!!</h2>
+        <h2>Game Over!!</h2>
         {winner && <p>{winner} won!</p>}
         {!winner && <p>It's a draw</p>}
         <p>
